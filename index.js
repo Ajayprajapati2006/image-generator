254acb79f8585a34ddea104f8d59288c0bf9068c
Ajay prajapat <161003555+Ajayprajapati2006@users.noreply.github.com>
2025-03-17 18:10:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const client = new OpenAI({
     baseURL: "https://api.studio.nebius.com/v1/",
-    apiKey: "eyJhbGciOiJIUzI1NiIsImtpZCI6IlV6SXJWd1h0dnprLVRvdzlLZWstc0M1akptWXBvX1VaVkxUZlpnMDRlOFUiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDExODE3NTgyOTI3Nzc3MzQxMjkxMSIsInNjb3BlIjoib3BlbmlkIG9mZmxpbmVfYWNjZXNzIiwiaXNzIjoiYXBpX2tleV9pc3N1ZXIiLCJhdWQiOlsiaHR0cHM6Ly9uZWJpdXMtaW5mZXJlbmNlLmV1LmF1dGgwLmNvbS9hcGkvdjIvIl0sImV4cCI6MTg5OTg4NzMzNiwidXVpZCI6IjJhYmQxZGNjLWMxZDItNDE4OS1hZmUxLWNmMDcwZDVhYmRlMCIsIm5hbWUiOiJyYWplbmRyYSBqYW5naWQiLCJleHBpcmVzX2F0IjoiMjAzMC0wMy0xNlQxMDoyODo1NiswMDAwIn0._nxVmmuSvKZUIkxdbQdliHiL0MqDoxmQcI8iU67rUVk", // Ensure this is set in .env file
+    apiKey: "your-api-key", // Ensure this is set in .env file
 });
 
 app.post("/generate-image", async (req, res) => {
